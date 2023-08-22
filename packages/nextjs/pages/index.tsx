47c2a6367c54edf72790519f7a6bcfdbcec56a0f
Rostyslav <dg1h1t@protonmail.com>
2023-08-22 02:47:41 -0500

    Update header and home page text to "Slovo" instead of "Solo" and "buildspace project" instead of "Ethereum dev stack".

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
         <div className="px-5">
           <h1 className="text-center mb-8">
             <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Solo</span>
+            <span className="block text-4xl font-bold">Slovo</span>
             <span className="block text-2xl">A Buildspace Project</span>
           </h1>
           <p>
