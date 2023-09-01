8028c6c087c2c034076cadf51e0fe9841e1765d6
Rostyslav <dg1h1t@protonmail.com>
2023-09-01 00:49:55 -0500

    Add file upload and input fields for book cover, number of copies, charge per copy, and royalties

--- packages/nextjs/pages/publish.tsx
+++ packages/nextjs/pages/publish.tsx
@@ -21,15 +21,46 @@ const Publish: NextPage = () => {
       <div className="flex justify-center">
         <form className="space-y-4" onSubmit={handleSubmit}>
           <fieldset className="space-y-2">
-            <label className="block text-lg font-medium py-3">Upload your book file here</label>
+            <label className="block text-lg font-medium pb-3 pt-2">
+              Upload your <i>book</i> file here
+            </label>
             <input type="file" className="block" />
             <div className="w-full h-32 flex items-center justify-center bg-gray-200 border-2 border-dashed border-gray-400 text-gray-600 cursor-pointer hover:bg-gray-300">
               Drag & Drop File Here
             </div>
-            <div className="py-4">
+            {/* <div className="pt-3">
+              <hr />
+            </div> */}
+            <label className="block text-lg font-medium py-3">
+              Upload your book <i>cover</i> here
+            </label>
+            <input type="file" className="block" />
+            <div className="w-full h-32 flex items-center justify-center bg-gray-200 border-2 border-dashed border-gray-400 text-gray-600 cursor-pointer hover:bg-gray-300">
+              Drag & Drop File Here
+            </div>
+            <div className="pt-3">
               <hr />
             </div>
           </fieldset>
+          <fieldset className="space-y-2">
+            <label className="block text-lg font-medium py-3">
+              How many <i>copies</i> do you want to publish?
+            </label>
+            <input type="number" className="block dark:text-black pl-2" min="1" max="10000" />
+            <label className="block text-lg font-medium py-3">
+              How much do you want to <i>charge</i> for each copy?
+            </label>
+            <span className="mr-2 inline">$</span>
+            <input type="number" className="inline dark:text-black pl-2" min="1" max="100" />
+            <label className="block text-lg font-medium py-3">
+              How much <i>royalties</i> do you want to have with each resale?
+            </label>
+            <span className="mr-2 inline">%</span>
+            <input type="number" className="inline dark:text-black pl-2" min="0" max="30" step="0.01" />
+          </fieldset>
+          <div className="pt-3">
+            <hr />
+          </div>
           <button type="submit" className="py-2 px-4 bg-blue-500 text-white rounded">
             Submit
           </button>
