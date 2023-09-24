760975028acd8b29b66c721cd72c44bcbc629071
Rostyslav <dg1h1t@protonmail.com>
2023-09-23 18:44:54 -0700

    Add logic to fetch and update book metadata

--- packages/nextjs/pages/buy.tsx
+++ packages/nextjs/pages/buy.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import type { NextPage } from "next";
+import { toGatewayURL } from "nft.storage";
 import { MetaHeader } from "~~/components/MetaHeader";
 import { useScaffoldContractWrite, useScaffoldEventHistory } from "~~/hooks/scaffold-eth";
 
@@ -11,12 +12,15 @@ type BookMetadata = {
   symbol: string;
   price: number;
   baseURI: string;
+  priceInDollars?: number; // new
+  description?: string; // new
+  image?: string; // new
 };
 
 const BuyPage: NextPage = () => {
   // const [purchased, setPurchased] = useState(false); // State to track if the NFT has been minted
   const [booksMetadata, setBooksMetadata] = useState<BookMetadata[]>([]);
-  const [fetchedMetadata, setFetchedMetadata] = useState<any[]>([]);
+  const [isMetadataFetched, setIsMetadataFetched] = useState(false);
 
   const { data, isLoading, error } = useScaffoldEventHistory({
     contractName: "BookFactory",
@@ -24,37 +28,30 @@ const BuyPage: NextPage = () => {
     fromBlock: process.env.NEXT_PUBLIC_DEPLOY_BLOCK ? BigInt(process.env.NEXT_PUBLIC_DEPLOY_BLOCK) : 0n, // replace with your starting block
   });
 
-  async function fetchMetadata(baseURI: string): Promise<any> {
+  async function fetchMetadata(baseURI: string): Promise<any | null> {
     try {
-      const res = await fetch(`https://ipfs.io/ipfs/${baseURI}`);
+      const { href } = await toGatewayURL(baseURI);
+      const res = await fetch(href);
+
       if (res.ok) {
-        const data = await res.json();
+        const data: any = await res.json();
         return data;
       } else {
         console.error(`Failed to fetch metadata. Status: ${res.status}`);
         return null;
       }
-    } catch (error) {
+    } catch (error: any) {
       console.error("Failed to fetch metadata:", error);
       return null;
     }
   }
 
   useEffect(() => {
-    const fetchAllMetadata = async () => {
-      const allPromises = booksMetadata.map(book => fetchMetadata(book.baseURI));
-      const allResults = await Promise.all(allPromises);
-      setFetchedMetadata(allResults);
-    };
-    console.log("fetchAllMetadata",  )
-  }, [booksMetadata]);
-
-  useEffect(() => {
     if (data && !isLoading && !error) {
       const newBooksMetadata = data.map(event => {
         return {
-          bookAddress: event.args.bookAddress,
           bookName: event.args.tokenName,
+          bookAddress: event.args.bookAddress,
           baseURI: event.args.baseURI,
           symbol: event.args.symbol,
           price: Number(event.args.price),
@@ -64,6 +61,42 @@ const BuyPage: NextPage = () => {
     }
   }, [data, isLoading, error]);
 
+  useEffect(() => {
+    const fetchAllMetadata = async () => {
+      const allPromises = booksMetadata.map(book => fetchMetadata(book.baseURI));
+      const allResults = await Promise.all(allPromises);
+
+      // Merge fetched metadata with existing metadata
+      const updatedBooksMetadata = booksMetadata.map((book, index) => {
+        const fetchedData = allResults[index];
+        if (fetchedData) {
+          return {
+            ...book,
+            priceInDollars: fetchedData.priceInDollars,
+            description: fetchedData.description,
+            symbol: fetchedData.symbol,
+            image: fetchedData.image,
+          };
+        }
+        return book;
+      });
+      setBooksMetadata(updatedBooksMetadata);// Update state
+    };
+
+    if (booksMetadata.length > 0 && !isMetadataFetched) {
+      // Added this condition
+      fetchAllMetadata();
+      setIsMetadataFetched(true);
+    }
+    // console.log("Books Metadata Updated:", booksMetadata);
+  }, [booksMetadata, isMetadataFetched]);
+
+  useEffect(() => {
+    if (isMetadataFetched) { // Only run this once after metadata has been fetched
+      console.log("Fetched Metadata:", booksMetadata);
+    }
+  }, [isMetadataFetched, booksMetadata]);
+
   return (
     <>
       <MetaHeader title="Mint HamletBook NFT" description="Mint your HamletBook NFT here." />
@@ -72,39 +105,18 @@ const BuyPage: NextPage = () => {
           {booksMetadata.map((book, index) => (
             <div className="flex " key={index}>
               Book Address: {book.bookAddress} <br />
-              Book URI: {book.baseURI}
-              <br />
               Book Name: {book.bookName} <br />
+              Description: {book.description} <br />
               Symbol: {book.symbol} <br />
-              Price: {book.price} wei <br />
-
-            ----------------------------------------
+              Book URI: {book.baseURI} <br />
+              Price in wei: {book.price} wei <br />
+              Price in dollars: ${book.priceInDollars} <br />
+              Image: {book.image} <br />
+              {/* <Image src={book.image} alt="Hamlet Book" width={200} height={300} /> */}
+              ----------------------------------------
             </div>
           ))}
         </div>
-
-        {/* <div className="flex flex-col gap-y-6 lg:gap-y-8 py-8 lg:py-12 justify-start items-center">
-          <Image
-            src="/hamlet.jpg" // Replace with the actual path to your image
-            alt="Hamlet Book" // Provide an appropriate alt description
-            width={200} // Adjust the width as needed
-            height={300} // Adjust the height as needed
-          />
-          <button className="btn btn-primary" onClick={handleMintClick}>
-            {purchased ? "Purchased" : "Buy"}
-            {purchased ? "" : <div>$15</div>}
-          </button>
-          {purchased ? (
-            <button className="btn btn-secondary">
-              <Link href="/read">Read</Link>
-            </button>
-          ) : (
-            ""
-          )}
-          <a href="/books/hamlet.pdf" target="_blank" rel="noopener noreferrer">
-            <button className="btn btn-secondary">Read a Book</button>
-          </a>
-        </div> */}
       </div>
     </>
   );
